030d6eeffddf130151b27b5321bbf229e5511606
Prakhar <Prakhar.agra10@gmail.com>
2024-04-12 11:28:40 +0530

    added header

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-content'>
+                <h1>Order your favorite food here</h1>
+                <p>Chose from a diverse menu featuring a delectable array of dishes</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    );
+}
+ 
+
+export default Header;
